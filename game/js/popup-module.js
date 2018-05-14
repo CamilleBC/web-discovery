a519faf5b3624ce3be381927744fa1832afa9b65
Camille Baillat <camille.baillat@protonmail.com>
2018-05-14 15:25:30 +0200

    [clean] mostly formatting and putting all js files to standard style.

--- game/js/popup-module.js
+++ game/js/popup-module.js
@@ -1,11 +1,16 @@
 // Popup module for the modal "loose" box
-var lostModal = document.getElementById('myModal');
-var close = document.getElementsByClassName("close")[0];
+var startupModal = document.getElementById('startup-screen')
+var close = document.getElementsByClassName('close')[0]
 
-close.onclick = function() {
-	lostModal.style.display = "none";
+window.onload = 'loadStartupScreen()'
+
+function loadStartupScreen () {
 }
 
-export function hello() {
-	return "Hello";
+close.onclick = function () {
+  lostModal.style.display = 'none'
+}
+
+export function hello () {
+  return 'Hello'
 }
